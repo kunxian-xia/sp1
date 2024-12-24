81c9af881682cf4fcc19e8aaff3dd194feeb1360
kunxian xia <xiakunxian130@gmail.com>
2024-12-24 22:39:41 +0800

    wip

--- crates/recursion/program/src/stark.rs
+++ crates/recursion/program/src/stark.rs
@@ -410,9 +410,9 @@ pub(crate) mod tests {
             println!("chip_ordering: {:?}", shard_proof.chip_ordering);
             for (chip, idx) in shard_proof.chip_ordering {
                 let opened_values = &shard_proof.opened_values.chips[idx];
-                println!("chip {} opened to {} main matrix", chip, opened_values.main.len());
-                println!("chip {} opened to {} preprocessed matrix", chip, opened_values.preprocessed.len());
-                println!("chip {} opened to {} perm matrix", chip, opened_values.permutation.len());
+                println!("chip {} opened to {} main matrix", chip, opened_values.main.local.len());
+                println!("chip {} opened to {} preprocessed matrix", chip, opened_values.preprocessed.local.len());
+                println!("chip {} opened to {} perm matrix", chip, opened_values.permutation.local.len());
                 println!("chip {} log_degree: {}", chip, opened_values.log_degree);
             }
         });
