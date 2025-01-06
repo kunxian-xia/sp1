485b6bc8990223c0de7794fd6285ba699906601e
kunxian xia <xiakunxian130@gmail.com>
2025-01-06 20:08:49 +0800

    print

--- crates/prover/src/lib.rs
+++ crates/prover/src/lib.rs
@@ -712,13 +712,6 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
                         }
                         let received = { proofs_rx.lock().unwrap().recv() };
                         if let Ok((index, height, vk, proof)) = received {
-                            tracing::info!(
-                                "received node {} at layer {} (tree height={}): proof size ={}",
-                                index,
-                                height,
-                                expected_height,
-                                bincode::serialized_size(&proof).unwrap(),
-                            );
                             batch.push((index, height, vk, proof));
 
                             // If we haven't reached the batch size, continue.
@@ -741,6 +734,11 @@ impl<C: SP1ProverComponents> SP1Prover<C> {
 
                             let is_complete = next_input_height == expected_height;
 
+                            tracing::info!(
+                                "sent input to generate node {} at layer {}",
+                                count,
+                                next_input_height
+                            );
                             let vks_and_proofs = inputs
                                 .into_iter()
                                 .map(|(_, _, vk, proof)| (vk, proof))
